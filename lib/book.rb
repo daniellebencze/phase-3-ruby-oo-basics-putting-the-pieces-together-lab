5839ed376ad24c8912dea97904dd1ef843d10ff6
Danielle Bencze <daniellebencze@live.com>
2022-02-26 14:28:57 -0800

    commit tree 58f0d04e8bd72d042d51f7ff852d01f742f46575

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
+class Book
+    attr_accessor :page_count, :author, :genre
+    attr_reader :title
 
+    def initialize (title)
+        @title= "And Then There Were None"
+        # @author= "Agatha Christie"
+        # @page_count = 272
+        # @genre = "Mystery"
+    end
+
+    def turn_page
+     puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
